527c9494cbca9fc622f3b097ae2185638435823b
Iris Fu <irisfu@fb.com>
2022-06-21 20:12:40 -0700

    Delete unecessary starter code

--- twitter/Views/TweetCell.m
+++ twitter/Views/TweetCell.m
@@ -86,17 +86,4 @@
     [self.retweetButton setSelected:self.tweet.retweeted];
 }
 
-
-
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
 @end
