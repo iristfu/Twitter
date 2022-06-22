623e38b57cc359652cce855b1bdfa2fb8aa757bd
Iris Fu <irisfu@fb.com>
2022-06-21 20:08:27 -0700

    Added retweet and unretweet functionality.

--- twitter/Views/TweetCell.m
+++ twitter/Views/TweetCell.m
@@ -20,10 +20,10 @@
         [self refreshFavoritesData];
         // Send a POST request to the POST unfavorite endpoint
         [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
-             if(error){
+             if (error) {
                   NSLog(@"Error unfavoriting tweet: %@", error.localizedDescription);
              }
-             else{
+             else {
                  NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
              }
          }];
@@ -34,21 +34,58 @@
         [self refreshFavoritesData];
         // Send a POST request to the POST favorite endpoint
         [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
-             if(error){
+             if (error) {
                   NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
              }
-             else{
+             else {
                  NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
              }
          }];
     }
-    
 }
 
 - (void)refreshFavoritesData {
     [self.favoriteButton setTitle:[NSString stringWithFormat:@"%d",self.tweet.favoriteCount] forState:UIControlStateNormal];
     [self.favoriteButton setSelected:self.tweet.favorited];
 }
+
+- (IBAction)didTapRetweet:(id)sender {
+    if (self.tweet.retweeted) { // user is unretweeting
+        self.tweet.retweetCount--;
+        self.tweet.retweeted = false;
+        // refresh UI
+        [self refreshRetweetsData];
+        // Send a POST request to the POST unretweet endpoint
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if (error) {
+                 NSLog(@"Error unretweeting tweet: %@", error.localizedDescription);
+            }
+            else {
+                NSLog(@"Successfully unretweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    } else { // user is retweeting
+        self.tweet.retweetCount++;
+        self.tweet.retweeted = true;
+        // refresh UI
+        [self refreshRetweetsData];
+        // Send a POST request to the POST retweet endpoint
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if (error) {
+                 NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else {
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+}
+
+- (void)refreshRetweetsData {
+    [self.retweetButton setTitle:[NSString stringWithFormat:@"%d", self.tweet.retweetCount] forState:UIControlStateNormal];
+    [self.retweetButton setSelected:self.tweet.retweeted];
+}
+
 
 
 - (void)awakeFromNib {
